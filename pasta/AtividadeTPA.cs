5127ecccc2b401527b3602d21f8e3165f5af34e0
la-ris-sa <carvalh06.la@gmail.com>
2022-11-18 11:58:56 -0300

    BAdaroksa

---
+++ pasta/AtividadeTPA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_TPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int pt = 0;
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("Digite o nome de um time de futebol:");
+                nome = Console.ReadLine();
+   
+                Console.WriteLine("Digite o ponto desse time:");
+                pt = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
